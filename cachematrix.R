c4a958b4d9e4dd6e7b82515f31f5d0aa43336313
Kavitha1Bhukya <kavitha.bhukya@sanofi.com>
2025-08-07 10:20:45 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,45 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+  inv<- NULL 
+  set <-function(y){
+    x<<- y
+    inv<<-NULL
+  }
+  get <-function()x
+  setinverse <-function(inverse) inv <<-inverse
+  getinverse<-function() inv
+  list(set=set, get=get,
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+ 
+cacheinverse<- function(x, ...){
+  inv<- x$getinverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  matrix_to_inv_data<-x$get()
+  inv<-solve(matrix_to_inv_data, ...)
+  x$setinverse(inv)
+  inv
 }
+my_Matrix<-makeCacheMatrix(matrix(1:4,2,2))
+my_Matrix$get()
+my_Matrix <- makeCacheMatrix(matrix(c(1,2,3,4), 2, 2))
+my_Matrix <- makeCacheMatrix(matrix(c(4,3,2,1), 2, 2))  # Using an invertible matrix
+result <- cacheinverse(my_Matrix)  # This will compute and store the inverse
+print(result)  # This should show the inverse matrix
+cacheinverse(my_Matrix)
+ 
+result2 <- cacheinverse(my_Matrix)  # This should retrieve from cache and show the message
+
+
+
